a6f7524fdc9fde78475d0dcb98855a996e7eb850
Ching-Ting-Wei <tim149557@gmail.com>
2023-11-16 04:33:49 +0800

    connect BE

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    require_once("conn.php");
+    $result = $conn->query("SELECT * FROM comments ORDER BY id DESC");
+    if(!$result){
+        die('Error' . $conn->error);
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -23,19 +31,24 @@
             </form>
             <div class="board__hr"></div>
             <section>
+                <?php
+                    while($row = $result->fetch_assoc()){
+
+                    
+                ?>  
                 <div class="card">
-                    <div class="card__avatar">
-                    </div>
+                    <div class="card__avatar"></div>
                     <div class="card__body">
                         <div class="card__info">
-                            <span class="card__author">Jacky</span>
-                            <span class="card__time">2023-11-16 11:11:11</span>
+                            <span class="card__author"><?php echo $row['nickname']; ?></span>
+                            <span class="card__time"><?php echo $row['created_at']; ?></span>
                         </div>
                         <p class="card__content">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam egestas sapien odio, in luctus erat cursus sed. Nam id erat iaculis, tempus sapien id, accumsan arcu. Sed ac libero in mauris imperdiet pellentesque. Sed vulputate, lectus nec luctus euismod, lectus erat iaculis felis, vitae viverra magna nulla vitae justo. In nisi arcu, sagittis id nibh id, viverra consectetur ipsum. Nunc luctus quam sit amet ipsum porta scelerisque. Ut ut pellentesque arcu. Mauris iaculis leo at condimentum pulvinar.
                         </p>
                     </div>
                 </div>
+                <?php } ?>
             </section>
          </main>
     </body>
